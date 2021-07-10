20aa45ae60a70bcc550eb6cf74539ff8ae9e8286
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-07-10 14:32:33 -0400

    Added saving a note by clicking the Save icon

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const { notes } = require("./db/notes");
-// const { renderNoteList } = require("./public/assets/js/index");
+// const { saveNote } = require("./public/assets/js/index");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -20,13 +21,30 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-  });
+app.get("/api/notes", (req, res) => {
+  const notesJSON = fs.readFileSync("./db/notes.json", "utf8");
+  console.log(notesJSON);
+  const {notes} = JSON.parse(notesJSON);
+  res.json(notes);
+});
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  note.id = Math.random().toString(36).slice(2);
+  const notesJSON = fs.readFileSync("./db/notes.json", "utf8");
+  console.log(notesJSON);
+  const {notes} = JSON.parse(notesJSON);
+
+  console.log("Notes: ", notes);
+  console.log("Note: ", note);
+  notes.push(note);
+  fs.writeFileSync("./db/notes.json", JSON.stringify({notes}));
+  res.json(note);
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
